5578100bca2d0fef32c52a13681ea365ec7baedf
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-28 00:08:58 -0500

    api jar update

--- src/main/java/com/insurgencedev/deluxesellwandsaddon/listeners/DeluxeSellwandsEventListener.java
+++ src/main/java/com/insurgencedev/deluxesellwandsaddon/listeners/DeluxeSellwandsEventListener.java
@@ -4,28 +4,27 @@ import dev.norska.dsw.api.DeluxeSellwandSellEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
-import org.insurgencedev.insurgenceboosters.models.booster.GlobalBoosterManager;
-import org.insurgencedev.insurgenceboosters.settings.IBoostersPlayerCache;
+import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
 
 public final class DeluxeSellwandsEventListener implements Listener {
 
     @EventHandler
     public void onSell(DeluxeSellwandSellEvent event) {
-        String type = "Wand";
+        final String TYPE = "Sell";
         final String NAMESPACE = "DELUXE_SELLWANDS";
-        double totalMulti = 1;
+        final double[] totalMulti = {1};
 
-        IBoostersPlayerCache.BoosterFindResult pResult = IBoosterAPI.getCache(event.getPlayer()).findActiveBooster(type, NAMESPACE);
-        if (pResult instanceof IBoostersPlayerCache.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
         }
 
-        GlobalBoosterManager.BoosterFindResult gResult = IBoosterAPI.getGlobalBoosterManager().findBooster(type, NAMESPACE);
-        if (gResult instanceof GlobalBoosterManager.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
-        }
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            totalMulti[0] += globalBooster.getMultiplier();
+            return null;
+        }, () -> null);
 
-        event.setMoney(calculateAmount(event.getMoney(), totalMulti));
+        event.setMoney(calculateAmount(event.getMoney(), totalMulti[0]));
     }
 
     private double calculateAmount(double amount, double multi) {
